23bd0714e67d82a8b1073a378891ab1bbffb080d
liang@fedora <liang.ma@studenti.polito.it>
2016-06-05 21:21:51 +0200

    adapt python code to directed graphs

--- networkx/pNetwork.py
+++ networkx/pNetwork.py
@@ -8,15 +8,23 @@ import sys
 import json
 
 def main():
-    opts,args=getopt.getopt(sys.argv[1:],"p:")
-    fPath=opts[0][1]
+    opts,args=getopt.getopt(sys.argv[1:],"p:d")
+    direct=False
+    fPath=None
+    for tup in opts:
+        if '-p' in tup:
+            fPath=tup[1]
+        elif '-d' in tup:
+            direct=True
     filename=os.path.basename(fPath)
     fDir=os.path.dirname(fPath)
     sDir=os.path.join(fDir,"%s.fig"%filename)
     if not os.path.exists(sDir):
         os.mkdir(sDir)
-    with open(fPath,'rb') as fh:
-        G_analysis=nx.read_edgelist(fh)
+    if direct:
+        G_analysis=nx.read_edgelist(fPath,create_using=nx.DiGraph())
+    else:
+        G_analysis=nx.read_edgelist(fPath)
     gPath=os.path.join(sDir,"%s.graphml"%filename)
     nx.write_graphml(G_analysis,gPath)
     num_nodes=nx.number_of_nodes(G_analysis)
@@ -38,27 +46,27 @@ def main():
     #plt.show()
 
 ## clustering coefficient
-
-    plt.figure()
-    clusterCoef=nx.clustering(G_analysis)
-    plt.hist(clusterCoef.values(),bins=num_bins,normed=True)
-    plt.xlabel("clustering coefficient")
-    plt.ylabel("normalized quantity")
-    plt.title('Histogram of clustering coefficient distribution')
-    plt.grid(True)
-    plt.savefig(os.path.join(sDir,"clusteringCoefficientDistribution.jpg"))
-    #plt.show()
+    if not direct:
+        plt.figure()
+        clusterCoef=nx.clustering(G_analysis)
+        plt.hist(clusterCoef.values(),bins=num_bins,normed=True)
+        plt.xlabel("clustering coefficient")
+        plt.ylabel("normalized quantity")
+        plt.title('Histogram of clustering coefficient distribution')
+        plt.grid(True)
+        plt.savefig(os.path.join(sDir,"clusteringCoefficientDistribution.jpg"))
+        plt.show()
 
 ## components and sizes
-    components=nx.connected_components(G_analysis)
-    comp_size=list()
+        components=nx.connected_components(G_analysis)
+        comp_size=list()
 
-    for comp in components:
-        comp_size.append(len(comp))
-    Dict_comp={nx.number_connected_components(G_analysis):comp_size}
-    str_comp=json.dumps(Dict_comp)
-    with open(os.path.join(sDir,"components.json"),'w') as fh:
-        fh.write(str_comp)
+        for comp in components:
+            comp_size.append(len(comp))
+        Dict_comp={nx.number_connected_components(G_analysis):comp_size}
+        str_comp=json.dumps(Dict_comp)
+        with open(os.path.join(sDir,"components.json"),'w') as fh:
+            fh.write(str_comp)
 
 ## closeness
 
@@ -70,7 +78,7 @@ def main():
     plt.title('Histogram of closeness coefficient distribution')
     plt.grid(True)
     plt.savefig(os.path.join(sDir,"closenessDistribution.jpg"))
-    #plt.show()
+    plt.show()
 
 ## betweenness
     betweenness=nx.betweenness_centrality(G_analysis)
@@ -81,7 +89,7 @@ def main():
     plt.title('Histogram of betweenness coefficient distribution')
     plt.grid(True)
     plt.savefig(os.path.join(sDir,"betweennessDistribution.jpg"))
-    #plt.show()
+    plt.show()
 
 
 if __name__=="__main__":
